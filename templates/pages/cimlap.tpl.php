ffb637eb08e12eebf6fa2971e73527ad0f5e0095
Thuróczy Mátyás <thuroczy.matyas@gmail.com>
2021-04-02 11:47:43 +0200

    Tartalom feltöltése

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -1,3 +1,7 @@
 <h2>Ide jön majd a tartalom</h2>
 <P>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio aperiam veniam voluptatibus consectetur nobis libero amet? Voluptates officiis consequatur nisi qui, provident non soluta earum! Fugiat alias et quod hic?</p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem inventore labore animi porro atque deserunt totam minus ipsum corporis iure maiores repudiandae, voluptatum rerum suscipit temporibus? Tenetur hic eaque itaque.</p>
+<div>
+            <iframe class="video" width="420" height="300" src="https://www.youtube.com/embed/UJ7qFJOglfg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
