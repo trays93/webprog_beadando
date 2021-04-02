0e6874e96f224920ecee31119c1a4c88b07bccd9
Thuróczy Mátyás <thuroczy.matyas@gmail.com>
2021-04-02 15:39:24 +0200

    SQL script

--- templates/pages/belep.tpl.php
+++ templates/pages/belep.tpl.php
@@ -1,2 +1,10 @@
 <h2>Regisztráció/Belépés</h2>
 <p>2 gomb. belépés és reg. Default megejelnítés 2 box a belépéshez, ha reg akkor plusz a többi</p>
+<div class="row">
+   <div class="col-md-6">
+       <form action="">
+           bejelentkezés
+       </form>
+   </div>
+   <div class="col-md-6">afléjgnhaféogj</div>
+</div>
---
+++ webprog_beadando.sql
@@ -0,0 +1,21 @@
+﻿DROP TABLE IF EXISTS `reg_adat`;
+CREATE TABLE `reg_adat` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `veznev` varchar(40) DEFAULT NULL,
+  `cim` varchar(80) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `passw` varchar(32) DEFAULT NULL,
+  `kernev` varchar(40) DEFAULT NULL,
+  `login` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `uzenet`;
+CREATE TABLE `uzenet` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `uzenet` text DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `nev` varchar(40) DEFAULT NULL,
+  `targy` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
