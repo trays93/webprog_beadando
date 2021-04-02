34b8ce0a741c761b2855abd5d2b0e8a658a731e9
Thuróczy Mátyás <thuroczy.matyas@gmail.com>
2021-04-02 16:02:35 +0200

    Belép oldal

--- templates/pages/belep.tpl.php
+++ templates/pages/belep.tpl.php
@@ -1,10 +1,32 @@
-<h2>Regisztráció/Belépés</h2>
-<p>2 gomb. belépés és reg. Default megejelnítés 2 box a belépéshez, ha reg akkor plusz a többi</p>
 <div class="row">
    <div class="col-md-6">
-       <form action="">
-           bejelentkezés
+        <h2>Belépés</h2>
+        <form action="">
+            <label for="login" class="form-label">Login név:</label>
+            <input type="text" name="login" id="login" class="form-control is-valid" required>
+            <label for="passw" class="form-label">Jelszó:</label>
+            <input type="password" name="passw" id="passw" class="form-control is-valid" required>
+            <br>
+            <button class="btn btn-primary" type="submit">Belépés</button>
        </form>
    </div>
-   <div class="col-md-6">afléjgnhaféogj</div>
+   <div class="col-md-6">
+        <h2>Regisztráció</h2> 
+        <form action="">
+            <label for="veznev" class="form-label">Vezeték név</label>
+            <input type="text" name="veznev" id="veznev" class="form-control is-valid" required>
+            <label for="kernev" class="form-label">Kereszt név:</label>
+            <input type="text" name="login" id="login" class="form-control is-valid" required>
+            <label for="login" class="form-label">Login név:</label>
+            <input type="text" name="login" id="login" class="form-control is-valid" required>
+            <label for="email" class="form-label">E-mail cím:</label>
+            <input type="email" name="email" id="email" class="form-control is-valid" required>
+            <label for="passw" class="form-label">Jelszó:</label>
+            <input type="password" name="passw" id="passw" class="form-control is-valid" required>  
+            <input type="checkbox" name="aszf" value="0" required>
+            <label class="form-label">Az általános szerződési feltételekt elfogadom.</label>
+            <br>
+            <button class="btn btn-primary" type="submit">Regisztráció</button>
+        </form>
+   </div>
 </div>
